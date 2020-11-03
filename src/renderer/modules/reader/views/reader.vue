ac7dc2c0d4ea9b0735205c31424576e13061e616
dengshaomin86 <1101869752@qq.com>
2020-11-03 22:53:32 +0800

    获取文件编码格式

--- src/renderer/modules/reader/views/reader.vue
+++ src/renderer/modules/reader/views/reader.vue
@@ -85,6 +85,7 @@
 <script>
   import fs from "fs";
   import path from "path";
+  import iconv from "iconv-lite";
   import jschardet from "jschardet";
   import {exec} from 'child_process';
   import {remote, ipcRenderer} from "electron";
@@ -202,8 +203,7 @@
           });
         }).catch(e => {
           console.log(e);
-          this.loading = false;
-          this.$message.error(e.message);
+          this.getBookContentByIconv(book);
         });
       },
 
@@ -218,6 +218,70 @@
             resolve(jschardet.detect(data).encoding);
           });
         });
+      },
+
+      // 转码方式获取文件内容
+      getBookContentByIconv(book) {
+        fs.readFile(book.url, {encoding: 'binary'}, (err, data) => {
+          if (err) {
+            this.loading = false;
+            console.log(err);
+            return;
+          }
+          if (!data) return;
+          let buf = new Buffer(data, 'binary');
+          let encodingType = this.encodingType(buf);
+          if (encodingType === "unicode") {
+            // this.$message.error("sorry，暂时无法解析 unicode 编码，请转换编码格式为ANSI或UTF8后再试");
+            this.getBookContentByXHR(book);
+            return;
+          }
+          let str = iconv.decode(buf, encodingType);
+          this.handlerBook(str);
+        });
+      },
+
+      // 根据 buffer 判断编码格式
+      encodingType(buf) {
+        if (buf[0] === 0xff && buf[1] === 0xfe) {
+          return "unicode";
+        } else if (buf[0] === 0xfe && buf[1] === 0xff) {
+          return "unicode";
+        } else if (buf[0] === 0xef && buf[1] === 0xbb) {
+          return "utf8";
+        }
+        return "GBK";
+      },
+
+      // xhr 获取书籍内容
+      getBookContentByXHR(book) {
+        let xhr = new XMLHttpRequest();
+        // 使用open方法初始化
+        // 第一个参数为请求方法 可以为GET、POST或其他
+        // 第二个参数为请求的地址
+        // 第三个参数为true 则为异步执行，为false 则为同步执行
+        xhr.open('GET', `file:/${book.url}`, true);
+        xhr.send(null);
+        xhr.onreadystatechange = () => {
+          switch (xhr.readyState) {
+            case 0:
+              // 此时对象尚未初始化，也没有调用open方法
+              break;
+            case 1:
+              // 此时对象已经调用了open方法，当没有调用send方法
+              break;
+            case 2:
+              // 此时调用了send方法，但服务器还没有给出响应
+              break;
+            case 3:
+              // 此时正在接收服务器的请求，当还没有结束，一般这里不做处理
+              break;
+            case 4:
+              // 此时已经得到了服务器放回的数据，可以开始处理
+              this.handlerBook(xhr.responseText);
+              break;
+          }
+        };
       },
 
       // 处理书籍内容
